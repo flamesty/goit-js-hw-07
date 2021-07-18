fb68a144e871838d85da93e6222b5937cc4e89cf
Vladyslav <79707548+litrekin@users.noreply.github.com>
2021-07-18 17:14:28 +0300

    src

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,17 +1,5 @@
-// Задание 8 - дополнительное, выполнять не обязательно
-// Напиши скрипт создания и очистки коллекции элементов.Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция.При нажатии на кнопку Очистить, коллекция элементов очищается.
-
-// Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число.Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
-
-// Каждый созданный div:
-
-// Имеет случайный rgb цвет фона
-// Размеры самого первого div - 30px на 30px
-// Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
-// Создай функцию destroyBoxes(), которая очищает div#boxes.
-
-const btnCreateRef = document.querySelector('button[data - action = "render"]');
-const btnDestroyRef = document.querySelector('button[data - action = "destroy"]');
+const btnCreateRef = document.querySelector('button[data-action="render"]');
+const btnDestroyRef = document.querySelector('button[data-action="destroy"]');
 const inputQntityRef = document.querySelector('div#controls input');
 const bigBox = document.querySelector('div#boxes');
 
@@ -24,7 +12,7 @@ function createBoxes(amount) {
         const addDiv = document.createElement("div");
         startWidth += 10;
         startHeight += 10;
-        addDiv.style = 'background - color: ' + '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase() + ';' + 'width: ${startWidth}px; height: ${startHeight}px;';
+        addDiv.style = 'background-color: ' + '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase() + ';' + `width: ${startWidth}px; height: ${startHeight}px;`;
         bigBox.appendChild(addDiv);
         console.log(bigBox.children);
     }
